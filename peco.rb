db9490cb00049b5b146f5b80f173a07e4087f49d
Daisuke Maki <lestrrat+github@gmail.com>
2014-06-12 14:18:31 +0900

    Initial commit
    I should use godep, but I couldn't make it work. Punting it for now

---
+++ peco.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+HOMEBREW_PECO_VERSION='0.1.0'
+class Peco < Formula
+  homepage 'https://github.com/lestrrat/peco'
+  url 'https://github.com/lestrrat/peco.git', :tag => "v#{HOMEBREW_PECO_VERSION}"
+  version HOMEBREW_PECO_VERSION
+  head 'https://github.com/lestrrat/peco.git', :branch => 'master'
+
+  depends_on 'go' => :build
+  depends_on 'hg' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    mkdir_p buildpath/'src/github.com/lestrrat'
+    ln_s buildpath, buildpath/'src/github.com/lestrrat/peco'
+    system 'go', 'get', 'github.com/jessevdk/go-flags'
+    system 'go', 'get', 'github.com/mattn/go-runewidth'
+    system 'go', 'get', 'github.com/nsf/termbox-go'
+    system 'go', 'build', 'cmd/peco/peco.go'
+    bin.install 'peco'
+  end
+end
