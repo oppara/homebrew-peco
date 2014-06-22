83dd534a2523fca166c34a730b96d30fbf8e934f
Daisuke Maki <lestrrat+github@gmail.com>
2014-06-22 09:58:54 +0900

    update version
    From this version, peco no longer needs to build up brew install/uprade,
    as we just download from github assets

--- peco.rb
+++ peco.rb
@@ -1,23 +1,16 @@
 require 'formula'
 
-HOMEBREW_PECO_VERSION='0.1.6'
+HOMEBREW_PECO_VERSION='0.1.7'
 class Peco < Formula
   homepage 'https://github.com/lestrrat/peco'
-  url 'https://github.com/lestrrat/peco.git', :tag => "v#{HOMEBREW_PECO_VERSION}"
+  url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_darwin_amd64.zip"
+
   version HOMEBREW_PECO_VERSION
   head 'https://github.com/lestrrat/peco.git', :branch => 'master'
 
-  depends_on 'go' => :build
-  depends_on 'hg' => :build
+  depends_on 'unzip' => :build
 
   def install
-    ENV['GOPATH'] = buildpath
-    mkdir_p buildpath/'src/github.com/lestrrat'
-    ln_s buildpath, buildpath/'src/github.com/lestrrat/peco'
-    system 'go', 'get', 'github.com/jessevdk/go-flags'
-    system 'go', 'get', 'github.com/mattn/go-runewidth'
-    system 'go', 'get', 'github.com/nsf/termbox-go'
-    system 'go', 'build', 'cmd/peco/peco.go'
     bin.install 'peco'
   end
 end
