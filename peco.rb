373e08eccb2ddee09658a751c27f3bd0df537c6c
Daisuke Maki <lestrrat+github@gmail.com>
2014-06-23 20:58:57 +0900

    oops, fix urls

--- peco.rb
+++ peco.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 HOMEBREW_PECO_VERSION='0.1.7'
 class Peco < Formula
-  homepage 'https://github.com/lestrrat/peco'
+  homepage 'https://github.com/peco/peco'
   url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_darwin_amd64.zip"
 
   version HOMEBREW_PECO_VERSION
-  head 'https://github.com/lestrrat/peco.git', :branch => 'master'
+  head 'https://github.com/peco/peco.git', :branch => 'master'
 
   depends_on 'unzip' => :build
 
