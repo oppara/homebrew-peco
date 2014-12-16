4034dc18bfaa4426a34861ca62c9f48d850f9339
Daisuke Maki <lestrrat+github@gmail.com>
2014-12-16 12:17:06 +0900

    Fix sha1

--- peco.rb
+++ peco.rb
@@ -6,10 +6,10 @@ class Peco < Formula
   homepage 'https://github.com/peco/peco'
   if OS.mac?
     url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_darwin_amd64.zip"
-    sha1 "b04f3ee8f5e43fa3b162981b50bb72fe1acabb33"
+    sha1 "1c92cfa7f2dc6d85f11c4225c1b2afff506b141f"
   elsif OS.linux?
     url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_linux_amd64.tar.gz"
-    sha1 "96e379fb98bd1b401c6ee52230c11d38251572cf"
+    sha1 "f83dc6c7334377d368cf0be43220639e3525874f"
   end
 
   version HOMEBREW_PECO_VERSION
