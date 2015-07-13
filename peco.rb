13ce9b196ddde94f38d5af61a96c0956fa7a251a
Daisuke Maki <lestrrat+github@gmail.com>
2015-07-13 10:50:41 +0900

    update version

--- peco.rb
+++ peco.rb
@@ -1,15 +1,15 @@
 # WARNING: Automatically generated. All changes to this file will be lost
 require 'formula'
 
-HOMEBREW_PECO_VERSION='0.3.1'
+HOMEBREW_PECO_VERSION='0.3.3'
 class Peco < Formula
   homepage 'https://github.com/peco/peco'
   if OS.mac?
     url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_darwin_amd64.zip"
-    sha1 "2fe563c7af4157e1ecd0b92686c86015dbb7ced0"
+    sha1 "fd35ea116c20b2ad8fd4935d0b0a0b1f4019ef7c"
   elsif OS.linux?
     url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_linux_amd64.tar.gz"
-    sha1 "20e33f7469b5c06c7e87ba6c0da21c4d5fa5fd37"
+    sha1 "e450acc3781139ebef3c25b1229abb3c052fd6b3"
   end
 
   version HOMEBREW_PECO_VERSION
