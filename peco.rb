8b3d76e2aef1a3f260d5c9a3ff0e86494e88e32d
oppara <oppara@oppara.tv>
2016-08-05 08:04:28 +0900

    Bump up peco version to 0.3.6

--- peco.rb
+++ peco.rb
@@ -1,15 +1,15 @@
 # WARNING: Automatically generated. All changes to this file will be lost
 require 'formula'
 
-HOMEBREW_PECO_VERSION='0.3.3'
+HOMEBREW_PECO_VERSION='0.3.6'
 class Peco < Formula
   homepage 'https://github.com/peco/peco'
   if OS.mac?
     url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_darwin_amd64.zip"
-    sha256 "7d2c56202f9b1273cd03b351e41e8a472c3b8bff9450a807ef77dc4e261dcb70"
+    sha256 "bec6be871d09ec15a1bb9fff3c6df548be30203beb5e07134af479c05ef50fac"
   elsif OS.linux?
     url "https://github.com/peco/peco/releases/download/v#{HOMEBREW_PECO_VERSION}/peco_linux_amd64.tar.gz"
-    sha256 "90728c44afbf63fe38cbdc54f6953c2d4187844288fb9a85f1ecd5c273e4ec94"
+    sha256 "f8b045bcaefbf4ffeee70b4ab2fc49263666f2d61f0b31a9ac2094870b8b3918"
   end
 
   version HOMEBREW_PECO_VERSION
